5f6095b397b41eb5dd6770f02055f6772f78cac3
harsh183 <hrshdeep51@gmail.com>
2018-12-11 07:18:56 -0600

    Make the app heroku ready

--- server.rb
+++ server.rb
@@ -310,49 +310,51 @@ def convert_to_ics(semester_schedule)
 end
 
 # For now just be a thin wrapper to java
-get '/' do
-  $distance_cache = {} # Store the results so that we don't end up hitting the server again and again
+class App < Sinatra::Base
+  get '/' do
+    $distance_cache = {} # Store the results so that we don't end up hitting the server again and again
 
-  file = File.open('Fall 2018 - Urbana-Champaign.ics') # MAHA TODO: Take file input
-  # Open a file or pass a string to the parse
-  cals = Icalendar::Calendar.parse(file)
-  cal = cals.first
-  events = cal.events
+    file = File.open('Fall 2018 - Urbana-Champaign.ics') # MAHA TODO: Take file input
+    # Open a file or pass a string to the parse
+    cals = Icalendar::Calendar.parse(file)
+    cal = cals.first
+    events = cal.events
 
-  # TODO: Un-hardcode it
-  start_date = Date.new(2018,8,29)
-  end_date = Date.new(2018,9,7) # should be 2018,12,12
+    # TODO: Un-hardcode it
+    start_date = Date.new(2018,8,29)
+    end_date = Date.new(2018,9,7) # should be 2018,12,12
 
-  schedule = SemesterSchedule.new(start_date, end_date)
+    schedule = SemesterSchedule.new(start_date, end_date)
 
-  # Look from start to end
-  current_date = start_date
-  while end_date >= current_date
-    # Exclude weekends
-    unless current_date.saturday? || current_date.sunday?
-      puts
-    	puts current_date
-      day_events = DayEvents.new(current_date)
-      day_events.pick_out_given_days_events(events)
+    # Look from start to end
+    current_date = start_date
+    while end_date >= current_date
+      # Exclude weekends
+      unless current_date.saturday? || current_date.sunday?
+        puts
+        puts current_date
+        day_events = DayEvents.new(current_date)
+        day_events.pick_out_given_days_events(events)
 
-      # puts "Day has #{day_events.student_events.length} events"
-      day_events.student_events.each do |event|
-        # puts event
+        # puts "Day has #{day_events.student_events.length} events"
+        day_events.student_events.each do |event|
+          # puts event
+        end
+
+        meal_timings = find_meal_timings(day_events)
+        day_events.student_events = meal_timings
+        schedule.add_new_day(day_events)
       end
-
-    	meal_timings = find_meal_timings(day_events)
-      day_events.student_events = meal_timings
-      schedule.add_new_day(day_events)
+      current_date = current_date.next_day
     end
-    current_date = current_date.next_day
+
+    ical_file = convert_to_ics(schedule)
+    # MAHA TODO: Return this back to use
+
+    # puts ical_file
+    ical_file
+    # Utility.new.get_final_string
   end
-
-  ical_file = convert_to_ics(schedule)
-  # MAHA TODO: Return this back to use
-
-  # puts ical_file
-  ical_file
-  # Utility.new.get_final_string
 end
 
 # MAHA TODO: Fix the time crossing bug (similar to java)
